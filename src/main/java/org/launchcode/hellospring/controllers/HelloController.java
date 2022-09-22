3cb972fa1a1d1996a2c9cefd105d60fbd0c52c67
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-09-22 08:32:31 -0500

    Initial commit

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,41 +3,52 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody
-@RequestMapping("hello")//sets the path all other mapping with be at hello/
 public class HelloController {
-    //handles request at /localhost8080/hello
-//now lives at hello/goodbye
-    @GetMapping("goodbye")
 
-    public String goodbye(){
+    //    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    //create a handler that handles requests of the form /hello?name=LaunchCode
-@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}) //How to ensure that it accepts get and post requests
-
-    public  String helloWithQuery(@RequestParam String name){
-        return "Hello, " + name +"!";
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
-//handler that handles requests of the form /hello/launchcode
-@GetMapping("{name}")
 
-    public String helloWithPathParam(@PathVariable String name){
-return "Hello, "+ name +"!";
-}
-@GetMapping("form")
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 
-    public String helloForm(){
+    @GetMapping("form")
+    @ResponseBody
+    public  String helloForm(){
         return "<html>" +
                 "<body>" +
-                "<form action= 'hello' method = 'post'>" +//submit a request to /hello
-                "<input type = 'text' name='name'>" +
+                "<form action = 'hello' method = 'post'>" + // submit request to hello
+                "<input type = 'text' name = 'name'>" +
                 "<input type = 'submit' value = 'Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+                "<form>" +
+                "<body>" +
+                "<html>";
     }
+
+
 
 }
