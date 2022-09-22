fe707fa371fc05394ea192394c1e21e8f5b237d0
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-09-22 10:11:38 -0500

    Cant get it to rturn properly

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("hello") //Should begin with /hello
 public class HelloController {
 
-    //    // Handle request at path /hello
+//        // Handle request at path /hello
 //    @GetMapping("hello")
 //    @ResponseBody
 //    public String hello() {
@@ -27,16 +27,55 @@ public class HelloController {
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
 //lives at /hello/hello
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam  String name, @RequestParam  String lg) {
+        if (lg == "French") {
+            return "Bon Jour " + name + "!";
+        } else if (lg == "English") {
+            return "Hello, " + name + "!";
+        } else if (lg == "Greek") {
+            return "Χαίρετε, " + name + "!";
+        } else if (lg == "Spanish") {
+            return "Hola, " + name + "!";
+        } else if (lg == "Cowboy") {
+            return "Howdy, " + name + "!";
+        } else if (lg == "Klingon") {
+            return "nuqneH, " + name + "!";
+        } else if (lg == "Elvish") {
+            return "S Suilad, " + name + "!";
+        } else {
+            return "You didnt choose a language.";
+        }
+
     }
+
 
     // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
+//    @GetMapping("{name}")
+//
+//    public String helloWithPathParam(@PathVariable String name) {
+//        return "Hello, " + name + "!";
+//    }
+//    public static String createMessage(String n, String lg) {
+//        String greeting = "";
+//
+//        if (lg.equals("english")) {
+//            greeting = "Hello";
+//        }
+//        else if (lg.equals("french")) {
+//            greeting = "Bonjour";
+//        }
+//        else if (lg.equals("italian")) {
+//            greeting = "Bonjourno";
+//        }
+//        else if (lg.equals("spanish")) {
+//            greeting = "Hola";
+//        }
+//        else if (lg.equals("german")) {
+//            greeting = "Hallo";
+//        }
+//
+//        return greeting + " " + n;
+//    }
 
     @GetMapping("form")
 //lives at /hello/form
@@ -45,10 +84,27 @@ public class HelloController {
                 "<body>" +
                 "<form action = 'hello' method = 'post'>" + // submit request to hello
                 "<input type = 'text' name = 'name'>" +
+                "<label for = 'lg'> Language </label>" +
+                "<select name = 'lg' id = 'lg'"+
+                "<option value = 'blank'> </option>" +
+                "<optgroup label=\"Real Languages\">" +
+                "<option value = 'lg'> French </option>" +
+                "<option value = 'lg'> English </option>" +
+                "<option value = 'Grk'> Greek </option>" +
+                "<option value = 'Sp'> Spanish </option>" +
+                "</optgroup>" +
+                "<optgroup label=\"Obscure Languages\">" +
+                "<option value = 'Cwby'> Cowboy </option>" +
+                "<option value = 'Klng'> Klingon </option>" +
+                "<option value = 'Elf'> Elvish </option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet me!'>" +
                 "<form>" +
                 "<body>" +
                 "<html>";
+
+
+
     }
 
 
